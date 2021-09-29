4f52b3fbf03d3f5118b5c13b1a254b5c49f0151b
angus-lewis <angus.lewis@student.adelaide.edu.au>
2021-09-29 16:18:27 +0930

    .

--- notebooks/preamble.jl
+++ notebooks/preamble.jl
@@ -1,6 +1,8 @@
 import Pkg
-Pkg.activate("..")
-Pkg.instantiate()
-Pkg.add(url="https://github.com/angus-lewis/DiscretisedFluidQueues")
+println(pwd())
+Pkg.activate("../.")
+Pkg.instantiate(verbose=true)
+# Pkg.add(url="https://github.com/angus-lewis/DiscretisedFluidQueues")
+Pkg.develop(url=pwd()*"/../../DiscretisedFluidQueues")
 using DiscretisedFluidQueues, Plots
-pyplot()
+# pyplot()
