8926a9412e456e94ddf8ad327bea9df55350bce2
angus-lewis <angus.lewis@student.adelaide.edu.au>
2021-12-06 17:11:42 +1030

    made a few plots for wave example (fun4, fun2)

--- wave/func_count_2/mesh_comp/figs/phd_ch_5_figs.jl
+++ wave/func_count_2/mesh_comp/figs/phd_ch_5_figs.jl
@@ -5,12 +5,12 @@ using JLD2
 using DiscretisedFluidQueues
 include("/Users/anguslewis/Documents/SFFMProject/DFQExamples.jl/plot_utils.jl")
 include("/Users/anguslewis/Documents/SFFMProject/DFQExamples.jl/error_metrics.jl")
-
+fun_number = 2
 @halfwidth_plot_defaults()
 
 plotlyjs()
 
-ks_data = CSV.read((@__DIR__)*"/../data/meshs_ks_func_count_4.csv",DataFrame)
+ks_data = CSV.read((@__DIR__)*"/../data/meshs_ks_func_count_$(fun_number).csv",DataFrame)
 
 p = plot()
 linestyles_vec = [:solid,:dash,:dashdot, :dot]
@@ -30,7 +30,7 @@ savefig((@__DIR__)*"/meshs_ks_error_formatted.pdf")
 
 
 
-l2_pdf_data = CSV.read((@__DIR__)*"/../data/meshs_l2_pdf_func_count_4.csv",DataFrame)
+l2_pdf_data = CSV.read((@__DIR__)*"/../data/meshs_l2_pdf_func_count_$(fun_number).csv",DataFrame)
 
 p = plot()
 linestyles_vec = [:solid,:dash,:dashdot, :dot]
@@ -48,9 +48,28 @@ plot!(legend=:outerbottomright)
 plot!()
 savefig((@__DIR__)*"/meshs_l2_pdf_error_formatted.pdf")
 
+
+l1_cdf_data = CSV.read((@__DIR__)*"/../data/meshs_l1_cdf_func_count_$(fun_number).csv",DataFrame)
+@halfwidth_plot_defaults()
+p = plot()
+linestyles_vec = [:solid,:dash,:dashdot, :dot]
+markerstyles_vec = [:cross,:diamond,:circle,:cross]
+for (c,col) in enumerate(names(l1_cdf_data))
+    plot!(1:2:21,l1_cdf_data[:,col],
+        label=col,linestyle=linestyles_vec[c],
+        marker=markerstyles_vec[c],
+        linewidth=2)
+end
+plot!(xlabel="Order")
+plot!(ylabel="log₁₀ error")
+plot!(title="L¹ error between true CDF and reconstruction")
+plot!(legend=:outerbottomright)
+plot!()
+savefig((@__DIR__)*"/meshs_l1_cdf_error_formatted.pdf")
+
 @fullwidth_plot_defaults()
 dir = @__DIR__()
-os = 1:2:7
+os = 9:2:15
 plot(layout=(4,length(os)))
 linestyles_vec = [:solid,:solid,:solid,:solid]
 markerstyles_vec = [:cross,:diamond,:circle]
@@ -61,9 +80,9 @@ for (c,o) in enumerate(os)
     mesh = DGMesh(0.0:10.0,o)
     dq = DiscretisedFluidQueue(model,mesh)
     jldopen(dir*"/../../../coeffs_matrix.jld2") do f 
-        coeffs = f["coeff_matrix"][4,c,1]
+        coeffs = f["coeff_matrix"][fun_number,c+4,1]
         dt = SFMDistribution(coeffs,dq)   
-        plot!(xs,pdf(dt).(xs,1),subplot=c,label=(c==1 ? "DG" : false),
+        plot!(xs,cell_probs(dt).(xs,1),subplot=c,label=(c==1 ? "DG" : false),
             linestyle=linestyles_vec[1],ylims = (-0.1,1.1),  yticks=false,
             # marker=markerstyles_vec[1],
             linewidth=2,xticks=false)
@@ -75,9 +94,9 @@ for (c,o) in enumerate(os)
     mesh = DGMesh(0.0:(1.0./o):10.0,1)
     dq = DiscretisedFluidQueue(model,mesh)
     jldopen(dir*"/../../../coeffs_matrix.jld2") do f 
-        coeffs = f["coeff_matrix"][4,c,2]
+        coeffs = f["coeff_matrix"][fun_number,c+4,2]
         dt = SFMDistribution(coeffs,dq) 
-        plot!(xs,pdf(dt).(xs,1),subplot=c+4,label=(c==1 ? "unif." : false),
+        plot!(xs,cell_probs(dt).(xs,1),subplot=c+4,label=(c==1 ? "unif." : false),
             linestyle=linestyles_vec[2],ylims = (-0.1,1.1),  yticks=false,
             # marker=markerstyles_vec[2],
             linewidth=2,xticks=false)
@@ -88,9 +107,9 @@ for (c,o) in enumerate(os)
     mesh = DGMesh(0.0:10.0,o)
     dq = DiscretisedFluidQueue(model,mesh)
     jldopen(dir*"/../../../coeffs_matrix.jld2") do f 
-        coeffs = f["coeff_matrix"][4,c,3]
+        coeffs = f["coeff_matrix"][fun_number,c+4,3]
         dt = SFMDistribution(coeffs,dq)   
-        plot!(xs,pdf(dt).(xs,1),subplot=c+8,label=(c==1 ? "DG (limiter)" : false),
+        plot!(xs,cell_probs(dt).(xs,1),subplot=c+8,label=(c==1 ? "DG (limiter)" : false),
             linestyle=linestyles_vec[3],ylims = (-0.1,1.1),  yticks=false,
             # marker=markerstyles_vec[3],
             linewidth=2,xticks=false)
@@ -102,9 +121,9 @@ for (c,o) in enumerate(os)
     mesh = FRAPMesh(0.0:10.0,o)
     dq = DiscretisedFluidQueue(model,mesh)
     jldopen(dir*"/../../../coeffs_matrix.jld2") do f 
-        coeffs = f["coeff_matrix"][4,c,4]
+        coeffs = f["coeff_matrix"][fun_number,c+4,4]
         dt = SFMDistribution(coeffs,dq)   
-        plot!(xs,pdf(dt).(xs,1),subplot=c+12,label=(c==1 ? "QBD-RAP" : false),
+        plot!(xs,cell_probs(dt).(xs,1),subplot=c+12,label=(c==1 ? "QBD-RAP" : false),
             linestyle=linestyles_vec[3],ylims = (-0.1,1.1), yticks=false,
             # marker=markerstyles_vec[3],
             linewidth=2,xticks=0:2:10, grid=false)
@@ -126,8 +145,9 @@ for (c,o) in enumerate(os)
     plot!(legend=false)
 end
 plot!() 
-savefig((@__DIR__)*"/pdfs_formatted.pdf")
+savefig((@__DIR__)*"/cell_probs_formatted.pdf")
 
+@halfwidth_plot_defaults()
 L1_cell_probs_errors = DataFrame(DG = Float64[], Order_1 = Float64[], DG_limiter = Float64[], QBDRAP = Float64[])
 truth = zeros(10)
 truth[5] = 1.0
@@ -141,7 +161,7 @@ for (c_o,o) in enumerate(1:2:21)
             mesh = (c_m<4) ? DGMesh(0.0:10.0,o) : FRAPMesh(0.0:10.0,o)
             dq = DiscretisedFluidQueue(model,mesh)
 
-            coeffs = f["coeff_matrix"][4,c_o,c_m]
+            coeffs = f["coeff_matrix"][fun_number,c_o,c_m]
             dt = SFMDistribution(coeffs,dq) 
 
             x_vals = range(0.5,9.5;length=10)
@@ -156,7 +176,7 @@ for (c_o,o) in enumerate(1:2:21)
     push!(L1_cell_probs_errors,row)
 end
 
-file = (@__DIR__)*"/../data/meshs_l1_cell_"*"func_count_4"
+file = (@__DIR__)*"/../data/meshs_l1_cell_"*"func_count_$(fun_number)"
 CSV.write(file*".csv",L1_cell_probs_errors)
 q = plot()
 linestyles_vec = [:solid,:dash,:dashdot, :dot]
@@ -168,8 +188,8 @@ for (c,reconstruction) in enumerate(names(L1_cell_probs_errors))
         linewidth=2)
 end
 plot!(xlabel="Order", ylabel="log₁₀ Error", 
-    title="Error between cell probabilities",
-    legend=:outertopright)
+    title="Error between cell masses",
+    legend=:outerbottomright)
 savefig((@__DIR__)*"/L1_cell_probs.pdf")
 
 # os = 1:2:7
