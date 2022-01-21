0e5eb2d47a036ccf95979d3053210758546ef153
angus-lewis <angus.lewis@student.adelaide.edu.au>
2022-01-21 14:28:36 +1030

    .

--- hitting_times_model/reflecting_model/transient_distribution/phases_1_2/point_mass/figs/phd_ch_5_figs.jl
+++ hitting_times_model/reflecting_model/transient_distribution/phases_1_2/point_mass/figs/phd_ch_5_figs.jl
@@ -35,7 +35,29 @@ plot!(legend=:outerbottomright)
 plot!()
 savefig((@__DIR__)*"/ks_error_formatted.pdf")
 
+L1_cell_probs_data = CSV.read((@__DIR__)*"/../data/L1_cell_probs.csv",DataFrame)
+L1_cell_probs_data_lwr = CSV.read((@__DIR__)*"/../data/L1_cell_probs_ci_lwr.csv",DataFrame)
+L1_cell_probs_data_upr = CSV.read((@__DIR__)*"/../data/L1_cell_probsci_upr.csv",DataFrame)
+@show lwr = [(L1_cell_probs_data[i,j]) .- (L1_cell_probs_data_lwr[i,j]) for i in 1:size(L1_cell_probs_data,1), j in 1:size(L1_cell_probs_data,2)]
+@show upr = [(L1_cell_probs_data_upr[i,j]) .- (L1_cell_probs_data[i,j]) for i in 1:size(L1_cell_probs_data,1), j in 1:size(L1_cell_probs_data,2)]
 
+p = plot()
+linestyles_vec = [:solid,:dash,:dashdot, :dot]
+markerstyles_vec = [:false,:false,:false,:false]
+_names = ["DG"; "DG (limit)"; "Unif"; "QBD-RAP"]
+colours = [1;3;2;4]
+for (c,col) in enumerate(names(L1_cell_probs_data))
+    plot!(1:2:21,L1_cell_probs_data[:,col], ribbon=(lwr[:,c],upr[:,c]),
+        label=_names[c],linestyle=linestyles_vec[c],
+        marker=markerstyles_vec[c],
+        linewidth=2,color=colours[c])
+end
+plot!(xlabel="Order")
+plot!(ylabel="log₁₀ error")
+plot!(title="Cell-wise error")
+plot!(legend=:outerbottomright)
+plot!()
+savefig((@__DIR__)*"/L1_cell_probs_error_formatted.pdf")
 
 l2_cdf_data = CSV.read((@__DIR__)*"/../data/L2.csv",DataFrame)
 l2_cdf_data_lwr = CSV.read((@__DIR__)*"/../data/L2_ci_lwr.csv",DataFrame)
